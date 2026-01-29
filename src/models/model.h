8c8ee72ae07b090d802361a310bee7a6c0546ef1
Álvaro Romero Castillo <alvaroromerocast@gmail.com>
2026-01-29 10:50:34 +0000

    added documentation and parallelisation

--- src/models/model.h
+++ src/models/model.h
@@ -12,6 +12,17 @@
 
 using ivec = std::vector<int>;
 
+/**
+ * @class Model
+ * @brief Base clase for different models
+ * 
+ * This class provides the core functionality for simulating statistical mechanical
+ * models (e.g., Ising, Potts, XY models) on lattices. It handles energy computations,
+ * partition functions, and state management.
+ * 
+ * 
+ * @tparam T data type used for lattice sites
+ */
 template <typename T>
 class Model: public ModelBase{
     protected:
@@ -21,24 +32,105 @@ class Model: public ModelBase{
 
 
     public: 
-        //TODO manage the bullshit with virutal functions and destructors,
+        //TODO manage the virutal functions and destructors,
         // copies, etc...
+
+        /**
+         * @brief Destroy the Model object
+         */
         virtual ~Model() =default;
+
+        /**
+         * @brief Default Constructor
+         * 
+         * Assigns the temperature and coupling constant to their private variables
+         * 
+         * @param temp 
+         * @param J 
+         */
         Model(double temp, double J)
             : ModelBase(),beta(1/temp), J(J), temp(temp){}
 
         // Class specific methods
+        /**
+         * @brief Computes the average energy of the system
+         * 
+         * @param energies array of energies to be averaged
+         * @return double value of average energy 
+         */
         double compute_average_energy(std::vector<T> energies);
+
+        /**
+         * @brief Computes the heat capacity of the system
+         * @detail TODO include formula
+         * 
+         * @param energies array of energies
+         * @return double 
+         */
         double compute_heat_capacity(std::vector<T> energies);
+
+        /**
+         * @brief Computes the partition function for a single configuration of the system
+         * @detail TODO INCLUDE FORMULA
+         * 
+         * @return double 
+         */
+
         double compute_single_partition_function() override;
+        
+        /**
+         * @brief Given an old configuration it reverts the system to that state
+         * 
+         */
+
         void revert_state_of_system() override {lattice_obj->get_lattice()=old_lattice;}
+        
+        /**
+         * @brief Given a DataSet it writes the current time step lattice configuration to it
+         * @detail After opening hte h5 file and the dataset, this function is called to write the current
+         * lattice configuration to it at the correct time step index.
+         * 
+         * @param[out] lattice_set a HighFive dataset object where the object is to be built
+         * @param[in] time 
+         */
+
         void write_lattice(HighFive::DataSet& lattice_set, int time) override;
 
         // Virtual functions
+        /**
+         * @brief Computes the spin term due to neighbouring spins for a given index
+         * 
+         * @return T total spiin
+         */
         virtual T compute_spin_neighbours_term(int index) = 0;
+
+        /**
+         * @brief Computes the spin term due to neighbouring spins for a given set of indices
+         * 
+         * @return T total spiin
+         */
         virtual T compute_spin_neighbours_term(ivec indices) = 0;
+
+        /**
+         * @brief computes the spin in the magnetic term of the hamiltonian for a given dimension
+         * 
+         * @param dim dimension along which to compute the magnetic term
+         * @return T total spin
+         */
         virtual T compute_spin_magnetic_term(int dim) = 0;
+
+        /**
+         * @brief computes the spin in the magnetic term of the hamiltonian for a single dimension model
+         * 
+         * @return T total spin
+         */
         virtual T compute_spin_magnetic_term() = 0;
+
+        /**
+         * @brief Given a site lattice index, it randomly changes the spin at that site
+         * 
+         * @param indices array of indices where the spins are to be changed
+         */
         virtual void change_spin_randomly(ivec indices) = 0;
 
 };
